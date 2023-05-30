da73a578ebdf09c971f326bd937c44687873a35a
stefano-morano <stefano.morano@mail.polimi.it>
2023-05-30 16:36:16 +0200

    first work and added the javadoc for the first method

--- api_2023.c
+++ api_2023.c
@@ -1,5 +1,43 @@
+/*
+ * Project for Algoritmi e Principi dell'Informatica
+ * AA 2023
+ * Prof. Matteo Pradella
+ * Name: CercaPercorso
+ * @author Stefano Morano 10737463
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * Function that adds a station to the railroad
+ * Return "aggiunta" if the station is added,
+ * return "non aggiunta" if the station already exists
+ */
+void aggiungi_stazione() {
+    //TODO
+}
+
+
+/**
+ * Function that removes a station from the railroad
+ * @param distance is the id of the station to be removed
+ */
+void demolisci_stazione(int distance) {
+    //TODO
+}
+
+void aggiungi_auto(){
+    //TODO
+}
+
+void rottama_auto(){
+    //TODO
+}
+
+void pianifica_percorso(){
+
+}
 
 int main() {
    //sum of the first five natural numbers
@@ -9,5 +47,5 @@ int main() {
         sum += input;
     }
     printf("The sum is: %d", sum);
-    //ueue
+    aggiungi_stazione();
 }
