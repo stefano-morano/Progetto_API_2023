8ada950f35e4e6cf3141071a72906abf11465551
stefano-morano <stefano.morano@mail.polimi.it>
2023-05-31 19:41:16 +0200

    Added the aggiungi_auto() and the aggiungi_stazione() con i loro metodi per farli funzionare correttamente. Ci sono ancora molti bug

--- api_2023.c
+++ api_2023.c
@@ -8,14 +8,87 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+
+typedef struct Node {
+    int distance;
+    int size;
+    int capacity;
+    struct Node *father;
+    struct Node *dx;
+    struct Node *sx;
+    int *car;
+} Node;
+
+typedef Node *station;
+station station_tree=NULL;
+
+char input[19];
+
+station in_station_position(station tree, int distance){
+    if (tree==NULL || tree->distance == distance)
+        return tree;
+            if (distance<tree->distance)
+                return in_station_position(tree->sx, distance);
+            else return in_station_position(tree->dx, distance);
+    }
+}
 /**
  * Function that adds a station to the railroad
  * Return "aggiunta" if the station is added,
  * return "non aggiunta" if the station already exists
  */
 void aggiungi_stazione() {
-    //TODO
+    int distance_input;
+    scanf("%d", &distance_input);                                               //read the distance of the new station
+    if (in_station_position(station_tree, distance_input)==NULL)
+        printf("non aggiunta\n");
+    else {
+        int num_input;
+        station radix=station_tree, scorr;
+        scanf("%d", &num_input);                                                   //read the initial number of cars in the station
+        station new_station = malloc(sizeof(Node) + num_input * sizeof(int));
+        new_station->distance=distance_input;
+        new_station->size = num_input;
+        new_station->capacity = num_input;
+        new_station->dx = NULL;
+        new_station->sx = NULL;
+        new_station->father = NULL;
+        for (int x = 0; x < num_input; x++) {
+            scanf("%d", &num_input);
+            *((new_station->car) + x) = num_input;                      //error: va in segmentation fault
+        }
+        if (station_tree==NULL){
+            station_tree=new_station;
+        } else {
+            while (station_tree!=NULL){
+                scorr=station_tree;
+                if (distance_input < station_tree->distance)
+                    station_tree=station_tree->sx;
+                else station_tree=station_tree->dx;
+            }
+            new_station->father=scorr;
+            if (distance_input < scorr->distance)
+                scorr->sx=new_station;
+            else scorr->dx=new_station;
+            new_station=radix;
+        }
+
+        printf("aggiunta\n");
+    }
+}
+
+//DEBUG
+void print_stations(station tree_to_print){
+    if (tree_to_print!=NULL){
+        print_stations(tree_to_print->sx);
+        printf("\nStazione alla distanza %d \nCon %d macchine ", tree_to_print->distance, tree_to_print->size);
+        for (int x=0; x<tree_to_print->size; x++)
+            printf("%d ", *((tree_to_print->car)+x));
+        printf("\n\n");
+        print_stations(tree_to_print->dx);
+    }
 }
 
 
@@ -27,8 +100,27 @@ void demolisci_stazione(int distance) {
     //TODO
 }
 
+int binary_search(){
+
+}
+
 void aggiungi_auto(){
-    //TODO
+    if (station_tree==NULL)
+        printf("Non aggiunta");
+    else {
+        int search_distance;
+        scanf("%d", &search_distance);
+        if (in_station_position(search_distance) == 1) {
+            while (station_tree->distance != search_distance)
+                if (search_distance < station_tree->distance)
+                    station_tree = station_tree->sx;
+                else station_tree = station_tree->dx;
+        }
+        if (station_tree->size == station_tree->capacity) {
+            station_tree->capacity *= 2;
+            station_tree->car = (int*) realloc(sizeof(int)*(station_tree->capacity));
+        }
+    }
 }
 
 void rottama_auto(){
@@ -40,12 +132,22 @@ void pianifica_percorso(){
 }
 
 int main() {
-   //sum of the first five natural numbers
-    int sum = 0, input;
-    for (int i = 1; i <= 5; i++) {
-        scanf("%d", &input);
-        sum += input;
-    }
-    printf("The sum is: %d", sum);
-    aggiungi_stazione();
+   do {
+       scanf("%s", input);
+       if (strcmp(input, "aggiungi-stazione") == 0)
+           aggiungi_stazione();
+       else if (strcmp(input, "distruggi-stazione") == 0)
+           printf("distruggi stazione\n");
+       else if (strcmp(input, "aggiungi-auto") == 0)
+           printf("Aggiungi auto\n");
+       else if (strcmp(input, "rottama-auto") == 0)
+           printf("rottama-auto\n");
+       else if (strcmp(input, "pianifica-percorso") == 0)
+           printf("pianifica percorso\n");
+       else if (strcmp(input, "stampa") == 0) {
+           if (station_tree==NULL)
+               printf("\nNessuna stazione presente\n");
+           else print_stations(station_tree);
+       }
+   } while (strcmp(input, "EOF")!=0);
 }
