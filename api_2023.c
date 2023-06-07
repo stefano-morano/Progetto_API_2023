7ea584b86bef3284a24e3ed4455a779cb5adc572
stefano-morano <stefano.morano@mail.polimi.it>
2023-06-07 18:06:05 +0200

    corrette alcune segmentation fualt dovute alla delete node e alla binary search

--- api_2023.c
+++ api_2023.c
@@ -12,7 +12,7 @@
 
 
 typedef struct Node {
-    unsigned int distance;
+    int distance;
     int size;
     int capacity;
     int last_visited;
@@ -37,6 +37,40 @@ station in_station_position(station tree, int distance){
     else return in_station_position(tree->dx, distance);
 }
 
+station first_station(station to_search){
+    while (to_search->sx!=NULL)
+        to_search=to_search->sx;
+    return to_search;
+}
+
+station last_station(station to_search){
+    while (to_search->dx!=NULL)
+        to_search=to_search->dx;
+    return to_search;
+}
+
+station previous_station(station to_previous){
+    if (to_previous->sx!=NULL)
+        return last_station(to_previous->sx);
+    station temp=to_previous->father;
+    while (temp!=NULL && to_previous == temp->sx){
+        to_previous=temp;
+        temp=temp->father;
+    }
+    return temp;
+}
+
+station next_station(station to_next){
+    if (to_next->dx!=NULL)
+        return first_station(to_next->dx);
+    station temp=to_next->father;
+    while (temp!=NULL && to_next == temp->dx){
+        to_next=temp;
+        temp=temp->father;
+    }
+    return temp;
+}
+
 void reset_array(){
     if (array_capacity!=0){
         for (int x=0; x<array_size; x++)
@@ -58,11 +92,9 @@ void insert_array(int distance){
 }
 
 int binary_search_right(station station_to_shift, int num_car){
-    int left = 0;
-    int right = station_to_shift->size - 1;
-
+    int left = 0, right = station_to_shift->size - 1, mid;
     while (left <= right) {
-        int mid = left + (right - left) / 2;
+        mid = (left + right) / 2;
         if (station_to_shift->car[mid] == num_car)
             return mid;
         else {
@@ -117,7 +149,7 @@ void aggiungi_stazione() {
             station_tree = radix;
         }
         if (new_station->capacity != 0) {
-            new_station->car = malloc(sizeof(int) * num_input);
+            new_station->car = (int*) malloc(sizeof(int) * num_input);
             int position;
             for (int x = 0; x < new_station->capacity; x++) {
                 if (scanf("%d", &num_input) <= 0)                                              //read the distance of the new station
@@ -132,6 +164,20 @@ void aggiungi_stazione() {
 
 //DEBUG
 void print_stations(station tree_to_print){
+    if (tree_to_print!=NULL){
+        print_stations(tree_to_print->sx);
+        printf("\nStazione alla distanza %d \nCon %d macchine ", tree_to_print->distance, tree_to_print->size);
+        if (tree_to_print->size!=0)
+            for (int x=0; x<tree_to_print->size; x++)
+                printf("%d ", tree_to_print->car[x]);
+        printf("\n\n");
+        print_stations(tree_to_print->dx);
+    }
+}
+
+//DEBUG
+void print_station(int distance){
+    station tree_to_print=in_station_position(station_tree, distance);
     if (tree_to_print!=NULL){
         print_stations(tree_to_print->sx);
         printf("\nStazione alla distanza %d \nCon %d macchine ", tree_to_print->distance, tree_to_print->size);
@@ -166,40 +212,57 @@ void shift_left(station station_to_shift, int position){
     station_to_shift->size--;
 }
 
-void delete_node(station node){
-    if (node->capacity>0)
-        free(node->car);
-    if (node->sx==NULL && node->dx==NULL){          //if the node is a leaf
-        if (node->father->sx==node)
-            node->father->sx=NULL;
-        else node->father->dx=NULL;
-    } else {
-        if (node->sx==NULL){
-            if (node->father->sx==node)
-                node->father->sx=node->dx;
-            else node->father->dx=node->dx;
+void copy_array(station source, station destination){
+    for (int x=0; x<source->size; x++)
+        destination->car[x]=source->car[x];
+}
+
+void delete_node(station node){                                         //da rivedere
+    if (node == NULL)
+        return;
+    Node* root = node->father;
+    if (node->sx == NULL) {
+        if (node->dx != NULL)
+            node->dx->father = node->father;
+
+        if (root != NULL) {
+            if (root->sx == node)
+                root->sx = node->dx;
+            else
+                root->dx = node->dx;
         } else {
-            if (node->dx==NULL){
-                if (node->father->sx==node)
-                    node->father->sx=node->sx;
-                else node->father->dx=node->sx;
-            } else {
-                station to_shift = node->dx;
-                while (to_shift->sx!=NULL)
-                    to_shift=to_shift->sx;
-                if (to_shift->father->sx==to_shift)
-                    to_shift->father->sx=NULL;
-                else to_shift->father->dx=NULL;
-                to_shift->father=node->father;
-                to_shift->sx=node->sx;
-                to_shift->dx=node->dx;
-                if (node->father->sx==node)
-                    node->father->sx=to_shift;
-                else node->father->dx=to_shift;
-            }
+            root = node->dx;
         }
+        if (node->capacity>0)
+            free(node->car);
+        free(node);
+    } else if (node->dx == NULL) {
+        node->sx->father = node->father;
+        if (root != NULL) {
+            if (root->sx == node)
+                root->sx = node->sx;
+            else
+                root->dx = node->sx;
+        } else {
+            root = node->sx;
+        }
+        if (node->capacity>0)
+            free(node->car);
+        free(node);
+    } else {
+        Node* next = node->dx;
+        while (next->sx != NULL)
+            next = next->sx;
+        node->distance = next->distance;
+        if (node->capacity==0)
+            node->car=(int*) malloc (sizeof(int)*next->capacity);
+        else if (next->capacity>node->capacity)
+            node->car=(int*) realloc (node->car, sizeof(int)*next->capacity);
+        node->capacity=next->capacity;
+        node->size=next->size;
+        copy_array(next, node);
+        delete_node(next);
     }
-    free(node);
 }
 
 void demolisci_stazione() {
@@ -224,7 +287,7 @@ void aggiungi_auto() {
         printf("non aggiunta\n");
     else {
         if (to_consider->capacity == 0) {
-            to_consider->car = malloc(sizeof(int));
+            to_consider->car = (int*) malloc(sizeof(int));
             to_consider->capacity = 1;
             to_consider->size = 1;
             if (scanf("%d", &to_consider->car[0]) <= 0)                                              //read the distance of the new station
@@ -235,14 +298,16 @@ void aggiungi_auto() {
                 if (scanf("%d", &to_consider->car[0]) <= 0)                                              //read the distance of the new station
                     return;
                 to_consider->size=1;
+                return;
             }
             if (to_consider->size == to_consider->capacity) {
                 to_consider->capacity *= 2;
                 (to_consider->car) = (int *) realloc (to_consider->car, sizeof(int) * to_consider->capacity);
             }
-            int position = binary_search_right(to_consider, distance_input), num_input;
-            if (scanf("%d", &num_input) <= 0)                                              //read the distance of the new station
+            int position, num_input;
+            if (scanf("%d", &num_input) <= 0)                                              //read the distance of the car to place
                 return;
+            position = binary_search_right(to_consider, num_input);
             shift_right(to_consider, position, num_input);
             printf("aggiunta\n");
         }
@@ -257,9 +322,9 @@ void rottama_auto(){
     if (to_consider==NULL) {
         printf("non rottamata\n");
     } else {
-        if (to_consider->capacity == 0 || to_consider->size == 0)
+        if (to_consider->capacity == 0 || to_consider->size == 0) {
             printf("non rottamata\n");
-        else {
+        } else {
             int num_input;
             if (scanf("%d", &num_input) <= 0)                                              //read the distance of the new station
                 return;
@@ -278,42 +343,10 @@ void rottama_auto(){
     }
 }
 
-station first_station(station to_search){
-    while (to_search->sx!=NULL)
-        to_search=to_search->sx;
-    return to_search;
-}
 
-station last_station(station to_search){
-    while (to_search->dx!=NULL)
-        to_search=to_search->dx;
-    return to_search;
-}
-
-station previous_station(station to_previous){
-    if (to_previous->sx!=NULL)
-        return last_station(to_previous->sx);
-    station temp=to_previous->father;
-    while (temp!=NULL && to_previous == temp->sx){
-        to_previous=temp;
-        temp=temp->father;
-    }
-    return temp;
-}
-
-station next_station(station to_next){
-    if (to_next->dx!=NULL)
-        return first_station(to_next->dx);
-    station temp=to_next->father;
-    while (temp!=NULL && to_next == temp->dx){
-        to_next=temp;
-        temp=temp->father;
-    }
-    return temp;
-}
 
 void afterward(station station_1, station station_2){
-    station temp_station = next_station(station_1), locked_station=station_1;
+   /* station temp_station = next_station(station_1), locked_station=station_1;
     do {
         while (temp_station!=NULL && locked_station->size!=0 && (locked_station->car[0] + locked_station->distance) >= temp_station->distance) {
             if (temp_station->last_visited == 0)
@@ -335,43 +368,46 @@ void afterward(station station_1, station station_2){
             }
             temp_station = in_station_position(station_tree, temp_station->last_visited);
         } while (temp_station->distance != station_1 -> distance);
-        for (int x=array_size-1; x>=0; x--) {
-            if (x == 0)
-                printf("%d\n", array_distance[x]);
-            else printf("%d ", array_distance[x]);
+        printf("%d", array_distance[array_size-1]);
+        for (int x=array_size-2; x>=0; x--) {
+            printf(" %d", array_distance[x]);
         }
-    } else printf("nessun percorso\n");
+        printf("\n");
+    } else printf("nessun percorso\n");*/
+    printf("\n");
 }
 
-void backward(station station_1, station station_2){
-    station temp_station = previous_station(station_1), locked_station=station_1;
+void backward(station station_2, station station_1){                                                                                                        //non va
+   /*station temp_station = next_station(station_1), locked_station=station_1;
     do {
-        while (temp_station!=NULL && locked_station->size!=0 && (locked_station->distance - locked_station->car[0]) <= temp_station->distance) {
-            if (temp_station->last_visited == 0)
+        while (temp_station!=NULL) {
+            if (temp_station->last_visited==0 && temp_station->size!=0 && ((temp_station->distance - temp_station->car[0]) <= locked_station->distance))
                 temp_station->last_visited = locked_station->distance;
-            temp_station = previous_station(temp_station);
+            temp_station = next_station(temp_station);
         }
-        locked_station = previous_station(locked_station);
-        temp_station = previous_station(locked_station);
+
+        locked_station = next_station(locked_station);
+        temp_station = next_station(locked_station);
     } while (locked_station->distance != station_2->distance);
     if (station_2->last_visited != 0){
         temp_station=station_2;
-        insert_array(station_2->distance);
+        insert_array(temp_station->distance);
         do {
-            if (temp_station->last_visited != 0)
+            if (temp_station->last_visited != 0) {
                 insert_array(temp_station->last_visited);
-            else {
+            } else {
                 printf("nessun percorso\n");
                 return;
             }
             temp_station = in_station_position(station_tree, temp_station->last_visited);
         } while (temp_station->distance != station_1 -> distance);
-        for (int x=array_size-1; x>=0; x--) {
-            if (x == 0)
-                printf("%d\n", array_distance[x]);
-            else printf("%d ", array_distance[x]);
+        printf("%d", array_distance[0]);
+        for (int x=1; x<array_size; x++){
+            printf(" %d", array_distance[x]);
         }
-    } else printf("nessun percorso\n");
+        printf("\n");
+    } else printf("nessun percorso\n");*/
+   printf("\n");
 }
 
 void reset_distance(station tree_to_reset){
@@ -403,7 +439,6 @@ void pianifica_percorso(){
 }
 
 int main() {
-    //FILE *file=freopen("output.txt", "w", stdout);
     while (scanf("%s", input)!=EOF){
         if (strcmp(input, "aggiungi-stazione") == 0)
             aggiungi_stazione();
@@ -421,5 +456,5 @@ int main() {
             else print_stations(station_tree);
         }
     }
-    //fclose(file);
+    return 0;
 }
